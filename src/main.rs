658fefd39d8f6ae36f64b4691d8fdd18070d8f86
Joshua D. Boyd <jdboyd@jdboyd.net>
2025-01-13 02:03:47 -0500

    Sort how out to include files correctly.
    Rename png_test to imgfile.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+use tempfile::NamedTempFile;
+
+mod imgfile;
+mod vec3;
+
 fn main() {
     println!("Hello, world!");
+
+    let temp_file = NamedTempFile::new().unwrap();
+    let rgb_data = vec![255, 0, 0, 0, 255, 0]; // 2x1 image: red and green pixels
+
+    let result = imgfile::write_rgb_to_png(temp_file.path(), 2, 1, &rgb_data);
+    assert!(result.is_ok());
 }
